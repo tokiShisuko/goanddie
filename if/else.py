327f4268041aa0ef1c17c2c9e2f2f904929e9b7d
Toki Shisuko <kueku5198@gmail.com>
2022-12-11 20:23:28 +0630

    if/else

---
+++ if/else.py
@@ -0,0 +1,11 @@
+my_age = 19
+my_friend_age = 18
+my_teacher_age = 27
+if my_age > my_friend_age:
+    print("I'm older than you.")
+elif my_teacher_age == my_friend_age:
+    print("My teacher is older than my friend.")
+elif my_friend_age < my_age:
+    print("I'm younger than my friend.")
+else:
+    print("UwU")
