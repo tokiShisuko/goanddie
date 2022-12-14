358b7ba9d8d240f8665f8c1de470ecfadda4289b
Toki Shisuko <kueku5198@gmail.com>
2022-12-14 14:00:09 +0630

    for homework

---
+++ if/git_hw.py
@@ -0,0 +1,16 @@
+first_num = int(input("Input your first number." ))
+second_num = int(input("Input your second number. "))
+if first_num > second_num:
+    print("First num is bigger than second num.")
+elif first_num < second_num:
+    print("First num is younger than second num.")
+elif second_num == first_num:
+    print("Both nums are same.")
+elif first_num != second_num:
+    print("They are not the same")
+else:
+    print("UwU")
+if first_num >= second_num:
+    print("Silent until we succceed!")
+if first_num <= second_num:
+    print("Respect to others!")
