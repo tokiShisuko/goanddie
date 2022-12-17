3f04f54f80fb4aea5b0bd81fa230703558581443
Toki Shisuko <kueku5198@gmail.com>
2022-12-17 19:50:22 +0630

    didn't finished

---
+++ if/test.py
@@ -0,0 +1,9 @@
+your_mark = int(input("Input your mark! "))
+if your_mark >= 80 and your_mark == 100:
+    print("You got grade A")
+if your_mark >= 60 and your_mark == 79 :
+    print("You got grade B")
+if your_mark >= 50 and your_mark == 59:
+    print("You got grade C")
+if your_mark < 40:
+    print("You fail the exam!")
