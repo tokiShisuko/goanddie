c76069860561e28d34e04d69be1eb66ff1eab3cf
Toki Shisuko <kueku5198@gmail.com>
2022-12-24 20:21:00 +0630

    first project

---
+++ First Project-main.py
@@ -0,0 +1,20 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image('00009:'
+                      '00090:'
+                      '90900:'
+                      '09000:'
+                      '0000:'))
+    sleep(5000)
+    display.show(Image('90009:'
+                        '09090:'
+                        '00900:'
+                        '09090:'
+                         '90009:'))
+    sleep(5000)
+   
+   
